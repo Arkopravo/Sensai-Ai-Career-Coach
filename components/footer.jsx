7e83eb91c05a3b9f33c4f559fce4de772814d3ae
Arkopravo Ghosh <arkopravoghosh789@gmail.com>
2025-02-19 12:45:36 +0530

    all done

---
+++ components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-muted/50 py-12" >
+
+            <div className="container mx-auto text-center px-4 text-gray-200">
+                <p>Made with 🧠 by Arkopravo Ghosh 😎</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
